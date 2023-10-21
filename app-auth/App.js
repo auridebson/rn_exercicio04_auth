d3ae56f9a287cdf9176d45edfc260e78386ace03
Auridebson Lima <auridebson@vutus.com>
2023-10-21 11:01:34 -0300

    Update

--- app-auth/App.js
+++ app-auth/App.js
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/Pages/Home'
+import Logado from './src/Pages/Logado'
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
