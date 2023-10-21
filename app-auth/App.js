63222fd7f12e1d35843e4e1c5f8acea8caa04722
Auridebson Sousa <auridebson@vutus.com>
2023-10-21 05:30:41 -0300

    Update

--- app-auth/App.js
+++ app-auth/App.js
@@ -1,12 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/Pages/Home'
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- app-auth/README.md
+++ app-auth/README.md
@@ -1,9 +1,8 @@
 Instalar a biblioteca de icons
 npm install @expo/vector-icons
 
-Instalar o Router DOM
-npm install react-router-dom
-
 Instalar o Navigation
 npm install @react-navigation/native
 npm install react-native-screens react-native-safe-area-context
+--Stack
+npm install @react-navigation/stack
